34c13f88e36ffbedbbacd9723562b0404f2051bb
Xinyuan Liu <lxy_pku@qq.com>
2016-04-09 23:27:42 +0800

    add burst

--- aFileChooserExample/src/main/java/com/ipaulpro/afilechooserexample/camera.java
+++ aFileChooserExample/src/main/java/com/ipaulpro/afilechooserexample/camera.java
@@ -2,10 +2,8 @@ package com.ipaulpro.afilechooserexample;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
@@ -17,7 +15,6 @@ import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.SurfaceHolder;
@@ -27,243 +24,221 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.SurfaceHolder.Callback;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-public class camera extends Activity
-        implements SurfaceHolder.Callback{
-    final private static String STILL_IMAGE_FILE = "capture.jpg";
-    final private static int IMAGE_NUM= 3;
-    private String strCaptureFilePath = "/sdcard/camera_snap.jpg";
-    private String TAG = "IsiCamera2Activity";
-    private Button btn_capture;
-    private Button btn_sum;
-    private Camera camera = Camera.open();
+public class camera extends Activity implements Callback, OnClickListener {
+    private SurfaceView mSurfaceView;
     private SurfaceHolder mSurfaceHolder;
-    private SurfaceView mSurfaceView01;
-    private FrameLayout mFrameLayout01;
-    private boolean bPreviewing =false;
-    private int intScreenWidth;
-    private int intScreenHeight;
+    private Camera mCamera;
+    private boolean mPreviewRunning;
+    private ImageView mImageView;
+    public String dir="/storage/sdcard1/test";
+    public String picpath="/storage/sdcard1/test.jpg";
+    boolean startflag=false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /* 使应用程序全屏幕运行，不使用title bar */
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        init();
+
+
+
+    }
+    private void init()
+    {
         setContentView(R.layout.camera);
-
-        /* 取得屏幕解析像素 */
-        getDisplayMetrics();
-        findViews();
-        getSurfaceHolder();
-        btn_capture.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                jumpOutAToast("test onclick");
-                //这里实现模拟连拍逻辑
-                for(int i=0; i<IMAGE_NUM; i++){
-                    takeAPicture();
-                }
-
-            }
-        });
-    }
-
-    @Override
-    protected void onPause() {
-        //被交换到后台时执行
-        //camera.release();
-        //camera = null;
-        bPreviewing = false;
-        jumpOutAToast("test onPause");
-        super.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        jumpOutAToast("test onStop");
-        resetCamera();
-        super.onStop();
-    }
-    @Override
-    protected void onResume() {
-        try {
-            initCamera();
-        } catch(IOException e ) {
-            Log.e(TAG,"initCamera() in Resume() erorr!");
-        }
-        jumpOutAToast("test onResume");
-        super.onResume();
-    }
-
-    /* function:
-     * 非preview时：实例化Camera,开始preview
-     * 非preview时and相机打开时：再设置一次preview
-     * preview时：不动作
-     */
-    private void initCamera() throws IOException
-    {
-        if(!bPreviewing)
-        {
-        /* 若相机非在预览模式，则打开相机 */
-            camera = Camera.open();
-            camera.setDisplayOrientation(90);
-        }
-        //非预览时and相机打开时，开启preview
-        if (camera != null && !bPreviewing)
-        {
-            Log.i(TAG, "inside the camera");
-        /* 创建Camera.Parameters对象 */
-            Camera.Parameters parameters = camera.getParameters();
-        /* 设置相片格式为JPEG */
-            parameters.setPictureFormat(PixelFormat.JPEG);
-        /* 指定preview的屏幕大小 */
-            parameters.setPreviewSize(intScreenWidth, intScreenHeight);
-        /* 设置图片分辨率大小 */
-            parameters.setPictureSize(intScreenWidth, intScreenHeight);
-        /* 将Camera.Parameters设置予Camera */
-            camera.setParameters(parameters);
-        /* setPreviewDisplay唯一的参数为SurfaceHolder */
-            camera.setPreviewDisplay(mSurfaceHolder);
-        /* 立即运行Preview */
-            camera.startPreview();
-            bPreviewing = true;
-        }
-    }
-    /* func:停止preview,释放Camera对象*/
-    private void resetCamera()
-    {
-        if (camera != null && bPreviewing)
-        {
-            camera.stopPreview();
-        /* 释放Camera对象 */
-            camera.release();
-            camera = null;
-            bPreviewing = false;
-        }
-    }
-    /* func:停止preview*/
-    private void stopPreview() {
-        if (camera != null && bPreviewing) {
-            Log.v(TAG, "stopPreview");
-            camera.stopPreview();
-        }
-    }
-    private void takeAPicture() {
-        if (camera != null && bPreviewing)
-        {
-          /* 调用takePicture()方法拍照 */
-            camera.takePicture
-                    (shutterCallback, rawCallback, jpegCallback);//调用PictureCallback interface的对象作为参数
-        }
-    }
-    /*func:获取屏幕分辨率*/
-    private void getDisplayMetrics() {
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        intScreenWidth = dm.widthPixels;
-        intScreenHeight = dm.heightPixels;
-        Log.i(TAG, Integer.toString(intScreenWidth));
-    }
-
-    private ShutterCallback shutterCallback = new ShutterCallback()
-    {
-        public void onShutter()
-        {
-            // Shutter has closed
-        }
-    };
-
-    private PictureCallback rawCallback = new PictureCallback()
-    {
-        public void onPictureTaken(byte[] _data, Camera _camera)
-        {
-            // TODO Handle RAW image data
-        }
-    };
-
-
-    private PictureCallback jpegCallback = new PictureCallback()
-    {
-        public void onPictureTaken(byte[] _data, Camera _camera)
-        {
-            //保存
-            Bitmap  bitmap=BitmapFactory.decodeByteArray(_data, 0, _data.length);
-            if(bitmap!=null){
-                File myCaptureFile=new File("/sdcard/test.jpg");
-
-                BufferedOutputStream bos=null;
-                try {
-                    bos = new BufferedOutputStream(new FileOutputStream(myCaptureFile));
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bos);
-                    bos.close();
-                    bitmap.recycle();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            resetCamera();
-            try {
-                initCamera();
-            } catch(Exception e) {
-                Log.e(TAG, "initCamera Error after snapping");
-            }
-        }
-    };
-    /* get a fully initialized SurfaceHolder*/
-    private void getSurfaceHolder() {
-        mSurfaceHolder = mSurfaceView01.getHolder();
+        mSurfaceView = (SurfaceView) findViewById(R.id.camera);
+        mImageView = (ImageView) findViewById(R.id.image);
+        mImageView.setVisibility(View.GONE);
+        mSurfaceView.setOnClickListener(this);
+        mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
         mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
-    /* 把SurfaceView 加到FrameLayout 里*/
-    private void addSurfaceViewToFrameLayout() {
-        mFrameLayout01.addView(mSurfaceView01);
-    }
-    /*func:弹出toast,主要做测试用 */
-    private void jumpOutAToast(String string) {
-        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
-    }
-    /* func:宣告界面元件Button capture,FrameLayout frame*/
-    private void findViews() {
-        btn_capture = (Button) findViewById(R.id.btn_capture);
-        btn_sum = (Button) findViewById(R.id.btn_sum);
-        /* 以SurfaceView作为相机Preview之用 */
-        mSurfaceView01 = (SurfaceView) findViewById(R.id.camera);
-    }
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        if(!bPreviewing) {
-            camera = Camera.open();
-            camera.setDisplayOrientation(90);
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        if (mPreviewRunning) {
+            mCamera.stopPreview();
         }
-    }
-    @Override
-    public void surfaceChanged(SurfaceHolder holder,int format,int width,int height) {
-        if(bPreviewing) {
-            camera.stopPreview();
-        }
-        Camera.Parameters params = camera.getParameters();
-        params.setPreviewSize(width, height);
-        camera.setParameters(params);
+        Parameters params = mCamera.getParameters();
+        params.setPictureFormat(PixelFormat.JPEG);// 设置图片格式
+        // params.setPreviewSize(width, height);
+        params.set("rotation", 90);
+        mCamera.setParameters(params);
         try {
-            camera.setPreviewDisplay(mSurfaceHolder);
-        } catch(IOException e) {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        camera.startPreview();
-        bPreviewing = true;
+        mCamera.startPreview();
+        mPreviewRunning = true;
     }
+
+    private AutoFocusCallback mAutoFocusCallBack = new AutoFocusCallback() {
+        @Override
+        public void onAutoFocus(boolean success, Camera camera) {
+            Log.v("AutoFocusCallback", "AutoFocusCallback , boolean success:"+ success);
+            Camera.Parameters Parameters = mCamera.getParameters();
+            Parameters.setPictureFormat(PixelFormat.JPEG);// 设置图片格式
+            mCamera.setParameters(Parameters);
+            mCamera.takePicture(mShutterCallback, null, mPictureCallback);
+        }
+    };
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mCamera = Camera.open();
+    }
+
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        camera.stopPreview();
-        bPreviewing = false;
-        camera.release();
-        mSurfaceHolder = null;
+        mCamera.stopPreview();
+        mPreviewRunning = false;
+        mCamera.release();
+        mCamera = null;
+    }
+
+
+
+
+
+    /**
+     * 拍照的回调接口
+     */
+    PictureCallback mPictureCallback = new PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.v("new", "pic call back…onPictureTaken…");
+            if (data != null) {
+                Log.i("new", "data != null");
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0,
+                        data.length);
+                Log.i("hack", picpath);
+                File myCaptureFile = new File(picpath);
+
+                try
+                {
+                    Log.i("new", "start photo");
+                    BufferedOutputStream bos=new BufferedOutputStream
+                            (new FileOutputStream(myCaptureFile));
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 80, bos);
+                    bos.flush();
+                    bos.close();
+                    bitmap.recycle();
+                    //mCamera.stopPreview();
+                    //mPreviewRunning = false;
+                    //mCamera.release();
+                    Log.i("new", picpath);
+                    //resetCamera();
+                    //Log.i("new", picpath);
+
+                }
+                catch (Exception e)
+                {
+                    Log.e("new", e.getMessage());
+                }
+
+
+                mCamera.stopPreview();
+                //mCamera.release();
+                startflag=true;
+                init();
+                //mCamera.startPreview();
+            }
+        }
+    };
+
+    private void resetCamera()
+    {
+        if (mCamera != null )
+        {
+            mCamera.stopPreview();
+       /* 扩展学习，释放Camera对象 */
+            mCamera.release();
+            mCamera = null;
+
+        }
+    }
+    /**
+     * 在相机快门关闭时候的回调接口，通过这个接口来通知用户快门关闭的事件，
+     * 普通相机在快门关闭的时候都会发出响声.
+     * 根据需要可以在该回调接口中定义各种动作， 例如：使设备震动
+     */
+    ShutterCallback mShutterCallback = new ShutterCallback() {
+        public void onShutter() {
+            // just log ,do nothing
+            Log.v("ShutterCallback", "…onShutter…");
+        }
+    };
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+            if (mCamera != null) {
+                // mCamera.takePicture(null, null,mPictureCallback);
+                mCamera.autoFocus(mAutoFocusCallBack);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onClick(View arg0) {
+        Log.v("new", "…onClick…");
+        startflag=false;
+        /*
+        for(int i=0;i<5;i++)
+        {
+            try{
+                Thread.sleep(1500);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            Log.i("hack",dir+Integer.toString(i)+".jpg");
+            // mCamera.autoFocus(mAutoFocusCallBack);
+            //Camera.Parameters Parameters = mCamera.getParameters();
+            //Parameters.setPictureFormat(PixelFormat.JPEG);// 设置图片格式
+            //mCamera.setParameters(Parameters);
+            mCamera.takePicture(mShutterCallback, null, mPictureCallback);
+        }*/
+        picpath=dir+0+".jpg";
+
+        mCamera.autoFocus(mAutoFocusCallBack);
+
+        Log.i("new", "finish photo");
+        new Thread(){
+            public void run()
+            {
+                for(int i=0;i<5;)
+                {
+                    picpath=dir+i+".jpg";
+                    try{
+                        Thread.sleep(2000);
+                    } catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                    if(startflag)
+                    {
+                        mCamera.autoFocus(mAutoFocusCallBack);
+                        startflag=false;
+                        i++;
+                    }
+                }
+
+            }
+        }.start();
+
+
+
+
     }
 }
-//}
