d92d6af575827fc080a95ddd944d3202acf7d497
ac_bull <bull@pku.edu.cn>
2016-04-10 00:13:37 +0800

    web view

--- aFileChooserExample/src/main/java/com/ipaulpro/afilechooserexample/camera.java
+++ aFileChooserExample/src/main/java/com/ipaulpro/afilechooserexample/camera.java
@@ -7,12 +7,9 @@ import android.os.Bundle;
  * Created by liuxinyuan on 16/4/9.
  */
 public class camera extends Activity{
-
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera);
     }
-
-
 }
